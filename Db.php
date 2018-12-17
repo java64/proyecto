6bbb6f332d05a87c38317c04b0e1605b98c68760
Miguel Angel <electro64.blog@gmail.com>
2018-12-17 18:20:08 +0100

    Subida Total

--- Db.php
+++ Db.php
@@ -1,19 +1,18 @@
 <?php
-    require 'vendor/autoload.php';
+	require 'vendor/autoload.php';
 
-class Db
-{
-    private static $conexion=null;
-    private function __construct()
-    {
-    }
+	class  Db{
+		private static $conexion=NULL;
+		private function __construct (){}
  
-    public static function conectar()
-    {
-        //Abrimos conexión a Mongo
-        $conexion = new MongoDB\Client;
-        //Seleccionamos base de datos hola
-        self::$conexion = $conexion->daw;
-        return self::$conexion;
-    }
-}
+		public static function conectar(){
+
+				//Abrimos conexión a Mongo
+				$conexion = new MongoDB\Client;
+				//Seleccionamos base de datos
+				self::$conexion = $conexion->daw;
+
+				return self::$conexion;
+		}		
+	} 
+?>
