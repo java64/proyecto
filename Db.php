f246f3b1903b4b81218fcca3f0d6d4f6f01e1330
Miguel Angel <electro64.blog@gmail.com>
2018-12-17 17:58:08 +0100

    Cambio en el comentario del constructor.

--- Db.php
+++ Db.php
@@ -1,18 +1,22 @@
 <?php
-	require 'vendor/autoload.php';
+    require 'vendor/autoload.php';
 
-	class  Db{
-		private static $conexion=NULL;
-		private function __construct (){}
+class Db
+{
+    private static $conexion=null;
+    private function __construct()
+    {
+    }
  
-		public static function conectar(){
+    public static function conectar()
+    {
 
-				//Abrimos conexión a Mongo
-				$conexion = new MongoDB\Client;
-				//Seleccionamos base de datos
-				self::$conexion = $conexion->daw;
+            //Abrimos conexión a Mongo
+            //Esto es un cambio
+            $conexion = new MongoDB\Client;
+            //Seleccionamos base de datos
+            self::$conexion = $conexion->daw;
 
-				return self::$conexion;
-		}		
-	} 
-?>
+            return self::$conexion;
+    }
+}
